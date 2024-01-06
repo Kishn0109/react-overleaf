e3e3aa99c46951699f90fbde36dbfe60787528e9
Kishan0109 <kishan.lr@codemonk.in>
2024-01-06 21:33:10 +0530

    feat: Implement useMapEvents hook

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  useMapEvents,
+} from "react-leaflet";
 import "./App.css";
+import { useState } from "react";
 function App() {
+  function LocationMarker() {
+    const [position, setPosition] = useState(null);
+    const map = useMapEvents({
+      click() {
+        map.locate();
+      },
+      locationfound(e) {
+        setPosition(e.latlng);
+        map.flyTo(e.latlng, map.getZoom(), {
+          duration: 3,
+        });
+      },
+    });
+
+    return position === null ? null : (
+      <Marker position={position}>
+        <Popup>You are here</Popup>
+      </Marker>
+    );
+  }
+
   return (
-    <div id="map" style={{ height: '100vh' ,width:"100%",border:"2px solid red" }}>
-      <MapContainer center={[51.505, -0.09]} zoom={13}  style={{width:"100%",height:"100%"}}>
+    <div
+      id="map"
+      style={{ height: "100vh", width: "100%", border: "2px solid red" }}
+    >
+      <MapContainer
+        center={{ lat: 51.505, lng: -0.09 }}
+        zoom={13}
+        style={{ width: "100%", height: "100%" }}
+      >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        <LocationMarker />
       </MapContainer>
     </div>
   );
